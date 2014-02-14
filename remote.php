fd19e8d98b9d4e19e88a6c018435273a0f674f66
Dominic Canare <dom@greenlightgo.org>
2014-02-14 00:02:58 -0600

    cleanup

--- remote.php
+++ remote.php
@@ -10,25 +10,6 @@
 	$teams = $backend->getTeams();
 	$players = indexBy($backend->getPlayers(), 'playerID');
 
-	if(!empty($_POST['teams'])){
-		
-	}
-
-	function generatePlayerSelect($name, $playerID, $gender){
-		global $players;
-		
-		$output = "<select name='$name'>";
-		$output .= "<option value=''></option>";
-		foreach($players as $id=>$player){
-			if($player['gender'] != $gender) continue;
-			
-			$selected = $playerID == $id ? " selected='1'" : '';
-			$output .= "<option value='$id'$selected>$player[firstName] $player[lastName]</option>";
-		}
-		$output .= "</select>";
-
-		return $output;
-	}
 ?>
 <!DOCTYPE html>
 <html>
@@ -46,36 +27,5 @@
 		</script>
 	</head>
 	<body>
-		<form method='post'>
-<?php
-
-if(count($teams) < 4){
-	for($i=0; $i<4; $i++){
-		$player1ID = null;
-		$player2ID = null;
-		if($i <= count($teams)){
-			$player1ID = $teams[$i]['player1ID'];
-			$player2ID = $teams[$i]['player2ID'];
-		}
-
-		echo "
-			<h3>Team " . ($i+1) . "</h3>
-			<table>
-				<tr>
-					<td>Player 1:</td>
-					<td>" . generatePlayerSelect("teams[$i][female]", $player1ID, 'female') . "</td>
-				</tr>
-				<tr>
-					<td>Player 2:</td>
-					<td>" . generatePlayerSelect("teams[$i][male]", $player2ID, 'male') . "</td>
-				</tr>
-			</table>
-			<hr/>";
-	}
-	echo "
-			<input type='submit' value='Save Teams' />";
-}
-?>
-		</form>
 	</body>
 </html>
